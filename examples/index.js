b00e26a130a3e7aa6d15fabc5afc9ec15559c34c
Ahmed Alaa <ahmed.elshoura23@gmail.com>
2020-07-08 21:12:07 +0200

    added add and drop tables

--- examples/index.js
+++ examples/index.js
@@ -4,15 +4,38 @@ import {setDB, dbOp} from '../lib/index.js'
 // creating MySql Pool
 let DB = mysql.createPool({
 	connectionLimit : 50,
-	host     : 'localhost',
-	user     : 'root',
-	password : '',
-	database : 'test',
-	timezone: 'UTC',
+	host			: 'localhost',
+	user			: 'root',
+	password	: '',
+	database	: 'test',
+	timezone	: 'UTC',
+	charset		: 'UTF8_GENERAL_CI',
 	multipleStatements: true
 });
 
 setDB(DB);
+
+// accounts: {
+// 	id: {type: 'INT(11)', autoIncrement: true, primaryKey: true, unique: true, allowNull: false},
+// 	name: {type: 'VARCHAR(255)', allowNull: false},
+// },
+// accounts_categories: {
+// 	id: {type: 'int(11)', isID: true},
+// 	name: {type: 'VARCHAR(255)'}
+// }
+
+const dbSchema = {
+	accounts: {
+		id: {type: 'INT(11)', isID: true},
+		name: {type: 'VARCHAR(255)'},
+	},
+	accounts_categories: {
+		id: {type: 'int(11)', isID: true},
+		name: {type: 'VARCHAR(255)'}
+	}
+}
+
+dbOp.createTables(dbSchema);
 
 async function addUser(){
 	// table name, data to insert
@@ -32,4 +55,4 @@ async function getUser(){
 }
 
 // addUser();
-getUser();
+// getUser();
